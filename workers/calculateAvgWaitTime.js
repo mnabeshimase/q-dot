0e158923e7755e2e57c80ced7123e38740ec5d48
Masaki Nabeshima <mnabeshimase@gmail.com>
2017-10-13 16:47:20 -0700

    Add worker file

---
+++ workers/calculateAvgWaitTime.js
@@ -0,0 +1,18 @@
+// const db = require('../database/index.js')
+//
+// db.Queue.findAll({
+//   where: {
+//     position: null,
+//     wait: null,
+//     updatedAt:
+//   }
+// })
+// .then((results) => {
+//   //results is an arra of quques
+//
+//   console.log(results);
+// })
+//
+//
+// // createdAt
+// // updatedAt
