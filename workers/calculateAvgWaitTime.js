7383003f3733e6a9968cf92c9e2f288beab019e3
Masaki Nabeshima <mnabeshimase@gmail.com>
2017-10-13 19:48:03 -0700

    Being implementing worker

--- workers/calculateAvgWaitTime.js
+++ workers/calculateAvgWaitTime.js
@@ -1,18 +1,45 @@
-// const db = require('../database/index.js')
-//
-// db.Queue.findAll({
-//   where: {
-//     position: null,
-//     wait: null,
-//     updatedAt:
-//   }
-// })
-// .then((results) => {
-//   //results is an arra of quques
-//
-//   console.log(results);
-// })
-//
-//
-// // createdAt
-// // updatedAt
+const db = require('../database/index.js')
+
+db.Queue.findAll({ //Find all entries that were dequeued within the last hour in Queues table
+  where: {
+    // position: null,
+    // wait: null,
+    updatedAt: {
+      [db.Sequelize.Op.gt]: new Date(new Date().getTime() - (1000*60*60))
+    }
+  }
+})
+.then((results) => {
+  let average_waits = {};//{ restaurant_id: Average wait time per person for the past hour }
+  for(let i = 0; i < results.length; i++) {
+    average_waits[results[i].dataValues.restaurantId] = average_waits[results[i].dataValues.restaurantId] || [];
+    average_waits[results[i].dataValues.restaurantId].
+    push((results[i].dataValues.updatedAt - results[i].dataValues.createdAt) / results[i].dataValues.size);
+  }
+  //Naive implementation: '(updatedAt - createdAt) / size' computes the time spent at the restaurant per peron.
+  for(let key in average_waits) {
+    average_waits[key] = average_waits[key].reduce((avg, current) => {
+      return avg + current / average_waits[key].length;
+    }, 0);
+  }
+  return db.LTWaits.find({
+    where: {
+      restaurant_id: {
+        [db.Sequelize.Op.in]: Object.getOwnPropertyNames(average_waits)
+      }
+    }
+  });
+})
+.then((results) => {
+  let updates = []
+  for(let i = 0; i < results.length; i++) {
+    results[i].dataValues.wait_avg.push(average_waits[results[i].dataValues.restaurant_id]);
+    updates.push(results[i].update({
+      wait_avg: results[i].dataValues.wait_avg
+    }))
+  }
+  return Promise.all(updates);
+})
+
+// createdAt
+// updatedAt
